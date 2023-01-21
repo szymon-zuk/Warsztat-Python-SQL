1ebd8ef30e82ccf7a852f5fac762d54c91057aa5
szymon-zuk <szymon.p.zuk@gmail.com>
2023-01-21 11:42:55 +0100

    obsłużony błąd OperationalError

--- create_db.py
+++ create_db.py
@@ -1,6 +1,7 @@
 import psycopg2
+from psycopg2 import OperationalError
 
-
+#tworzenie połączenia
 conn = psycopg2.connect(user="postgres",
                         password="coderslab",
                         host="localhost",
@@ -13,8 +14,8 @@ sql = '''CREATE DATABASE workshop_db;'''
 try:
     cursor.execute(sql)
     print("Baza danych została utworzona!")
-except psycopg2.errors.DuplicateDatabase:
-    print("Taka baza danych już istnieje!")
+except psycopg2.errors.DuplicateDatabase or OperationalError:
+        print("Podana baza danych już istnieje lub wystąpił problem z połączeniem!")
 
 sql_2 = '''CREATE TABLE users
 (
@@ -26,8 +27,8 @@ hashed_password varchar(80)
 try:
     cursor.execute(sql_2)
     print("Tablica 'users' została utworzona!")
-except psycopg2.errors.DuplicateTable:
-    print("Taka tablica już istnieje!")
+except psycopg2.errors.DuplicateTable or OperationalError:
+    print("Taka tablica już istnieje lub wystąpił problem z połączeniem!")
 
 sql_3 = '''CREATE TABLE messages
 (
@@ -43,8 +44,7 @@ FOREIGN KEY(to_id) REFERENCES users(id)
 try:
     cursor.execute(sql_3)
     print("Tablica 'messages' została utworzona!")
-except psycopg2.errors.DuplicateTable:
-    print("Taka tablica już istnieje!")
-
+except psycopg2.errors.DuplicateTable or OperationalError:
+    print("Taka tablica już istnieje lub wystąpił problem z połączeniem!")
 
 conn.close()
