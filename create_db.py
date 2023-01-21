da767d736c6e611bf1d44912c0e46fa28c7ddecd
szymon-zuk <szymon.p.zuk@gmail.com>
2023-01-21 11:23:41 +0100

    ukończony skrypt dodawania bazy danych oraz tablic users i messages

--- create_db.py
+++ create_db.py
@@ -4,7 +4,7 @@ import psycopg2
 conn = psycopg2.connect(user="postgres",
                         password="coderslab",
                         host="localhost",
-                        database="postgres")
+                        database="workshop_db")
 
 conn.autocommit = True
 cursor = conn.cursor()
@@ -15,5 +15,36 @@ try:
     print("Baza danych została utworzona!")
 except psycopg2.errors.DuplicateDatabase:
     print("Taka baza danych już istnieje!")
+
+sql_2 = '''CREATE TABLE users
+(
+id SERIAL PRIMARY KEY,
+username varchar(255),
+hashed_password varchar(80)
+);'''
+
+try:
+    cursor.execute(sql_2)
+    print("Tablica 'users' została utworzona!")
+except psycopg2.errors.DuplicateTable:
+    print("Taka tablica już istnieje!")
+
+sql_3 = '''CREATE TABLE messages
+(
+id SERIAL PRIMARY KEY,
+from_id INT,
+to_id INT,
+creation_date TIMESTAMP DEFAULT current_date,
+text varchar(255),
+FOREIGN KEY(from_id) REFERENCES users(id),
+FOREIGN KEY(to_id) REFERENCES users(id)
+);'''
+
+try:
+    cursor.execute(sql_3)
+    print("Tablica 'messages' została utworzona!")
+except psycopg2.errors.DuplicateTable:
+    print("Taka tablica już istnieje!")
+
 
 conn.close()
