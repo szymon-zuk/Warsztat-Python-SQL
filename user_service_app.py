2119b7adc1a4bef3c132ee268db1ca486e8d82c6
szymon-zuk <szymon.p.zuk@gmail.com>
2023-01-22 11:40:57 +0100

    funkcje: edit_user, delete_user, create_user, list_users

--- user_service_app.py
+++ user_service_app.py
@@ -1,6 +1,8 @@
 import argparse
 import psycopg2
-import models
+from models import User
+from psycopg2.errors import UniqueViolation
+from clcrypto import check_password
 
 conn = psycopg2.connect(user="postgres",
                         password="coderslab",
@@ -12,18 +14,53 @@ cursor = conn.cursor()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--username", help="username")
-parser.add_argument("-p", "--password", help="password")
-parser.add_argument("-n", "--new_pass", help="new_pass")
-parser.add_argument("-d", "--delete", help="delete")
-parser.add_argument("-e", "--edit", help="edit")
-parser.add_argument("-l", "--list", help="list")
-
+parser.add_argument("-p", "--password", help="password (min 8 characters)")
+parser.add_argument("-n", "--new_pass", help="new_pass (min 8 characters)")
+parser.add_argument("-d", "--delete", help="delete", action="store_true")
+parser.add_argument("-e", "--edit", help="edit", action="store_true")
+parser.add_argument("-l", "--list", help="list", action="store_true")
 
 args = parser.parse_args()
 
 
-def add_user(username, password):
-    db_username = models.User.load_user_by_username(cursor, username)
-    str_db_username = db_username.__str__()
-    if username == str_db_username:
+def list_users(cur):
+    users = User.load_all_users(cur)
+    for user in users:
+        print(user.username)
 
+
+def create_user(cur, username, password):
+    if len(password) < 8:
+        print("Password is too short! Type in min. 8 characters")
+    else:
+        try:
+            user = User(username=username, password=password)
+            user.save_to_db(cur)
+            print("User created!")
+        except UniqueViolation as e:
+            print("User already exists!", e)
+
+
+def delete_user(cur, username, password):
+    user = User.load_user_by_username(cur, username)
+    if user not in user.load_all_users():
+        print("User does not exist!")
+    elif check_password(password, user.hashed_password):
+        user.delete(cur)
+        print("User deleted!")
+    else:
+        print("Wrong password!")
+
+def edit_user(cur, username, password, new_pass):
+    user = User.load_user_by_username(cur, username)
+    if user not in user.load_all_users():
+        print("User does not exist!")
+    elif check_password(password, user.hashed_password):
+        if len(new_pass) < 8:
+            print("Password is too short!")
+        else:
+            user.hashed_password = new_pass
+            user.save_to_db(cur)
+            print("Password changed!")
+    else:
+        print("Wrong password!")
